4de942b2a5a3d884794fd3db15d7e0dda4d0a32c
JoeyHou <peterzhaoyi@163.com>
2018-08-06 16:15:49 +0800

    Finished lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -15,12 +15,49 @@ function initializePage() {
  	initRSVPForm();
 }
 
+// $(function(){
+// 	$("div.box").bind("taphold", tapholdHandler);
+// 	function tapholdHandler(event){
+// 		$(event.target).addClass("taphold");
+// 		console.log("hi");
+// 	}
+// });
+
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			// $(event.target).addClass("taphold");
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	// add your code here
+	$('#rsvpForm').submit(function(e){
+
+		e.preventDefault();
+		console.log("Submitting form...");
+		var rsvpEmail = $('#rsvpEmail').val();
+		// Send the POST request
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+
+	function postCallback(res){
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');//Clear form
+	}
+
+
+
+
+
 }
